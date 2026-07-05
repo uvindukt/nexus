756bdb29bdf496eb49567304edba600b32388888
Uvindu Sanjana <uvindu.sanjana@gmail.com>
2026-07-05 15:42:29 +0530

    WIP RAG retriever, query transformer, prompt template

--- services/rag/src/main/java/com/nexus/rag/application/dto/web/response/v1/ProductSearchResponse.java
+++ services/rag/src/main/java/com/nexus/rag/application/dto/web/response/v1/ProductSearchResponse.java
@@ -1,9 +1,15 @@
 package com.nexus.rag.application.dto.web.response.v1;
 
+import com.nexus.rag.domain.model.ProductStockView;
 import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+import java.util.List;
 
 @NullMarked
 public record ProductSearchResponse(
-
+        String answer,
+        @Nullable
+        List<ProductStockView> products
 ) {
 }
---
+++ services/rag/src/main/java/com/nexus/rag/application/service/ProductSearchServiceImpl.java
@@ -0,0 +1,56 @@
+package com.nexus.rag.application.service;
+
+import com.nexus.rag.application.dto.web.response.v1.ProductSearchResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.ToolCallingAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.preretrieval.query.transformation.QueryTransformer;
+import org.springframework.ai.rag.preretrieval.query.transformation.RewriteQueryTransformer;
+import org.springframework.ai.rag.retrieval.search.DocumentRetriever;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ProductSearchServiceImpl implements ProductSearchService {
+
+    @Value("classpath:/prompts/rag-no-context.st")
+    private Resource emptyContextResource;
+
+    private final ChatClient chatClient;
+    private final VectorStore vectorStore;
+
+    public ProductSearchServiceImpl(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
+
+        this.vectorStore = vectorStore;
+
+        DocumentRetriever retriever = VectorStoreDocumentRetriever.builder()
+                .vectorStore(vectorStore)
+                .similarityThreshold(0.75)
+                .topK(5)
+                .build();
+
+        QueryTransformer rewriteTransformer = RewriteQueryTransformer.builder()
+                .chatClientBuilder(chatClientBuilder.build().mutate())
+                .build();
+
+        Advisor ragAdvisor = RetrievalAugmentationAdvisor.builder()
+                .queryTransformers(rewriteTransformer)
+                .documentRetriever(retriever)
+                .build();
+
+        this.chatClient = chatClientBuilder.defaultAdvisors(ragAdvisor).build();
+
+    }
+
+    @Override
+    public ProductSearchResponse search(String userQuery) {
+        return null;
+    }
+
+}
