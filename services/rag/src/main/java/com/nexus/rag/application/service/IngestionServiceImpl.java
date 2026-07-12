0bf004d5629a125323e510edc95a4966b36a21f5
Uvindu Sanjana <uvindu.sanjana@gmail.com>
2026-07-12 19:18:29 +0530

    Qdrant ingestion pipeline update

--- services/rag/src/main/java/com/nexus/rag/application/service/IngestionServiceImpl.java
+++ services/rag/src/main/java/com/nexus/rag/application/service/IngestionServiceImpl.java
@@ -5,26 +5,39 @@ import com.nexus.rag.domain.exception.IngestionPipelineException;
 import com.nexus.rag.domain.model.ProductStockView;
 import com.nexus.rag.domain.repository.ProductEmbeddingRepository;
 import com.nexus.rag.domain.service.ProductHashCalculator;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.ai.vectorstore.filter.Filter;
 import org.springframework.ai.vectorstore.filter.FilterExpressionBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+import static com.nexus.rag.infrastructure.persistence.constant.RepositoryConstants.QDRANT_PRODUCT_EMBEDDING_REPOSITORY;
+
 @Slf4j
 @Service
-@RequiredArgsConstructor
 class IngestionServiceImpl implements IngestionService {
 
     private final ProductDocumentMapper productDocumentMapper;
     private final VectorStore vectorStore;
     private final ProductHashCalculator productHashCalculator;
     private final ProductEmbeddingRepository productEmbeddingRepository;
+
+    public IngestionServiceImpl(
+            ProductDocumentMapper productDocumentMapper,
+            VectorStore vectorStore,
+            ProductHashCalculator productHashCalculator,
+            @Qualifier(QDRANT_PRODUCT_EMBEDDING_REPOSITORY) ProductEmbeddingRepository productEmbeddingRepository
+    ) {
+        this.productDocumentMapper = productDocumentMapper;
+        this.vectorStore = vectorStore;
+        this.productHashCalculator = productHashCalculator;
+        this.productEmbeddingRepository = productEmbeddingRepository;
+    }
 
     @Override
     public void upsertEmbedding(ProductStockView view) {
