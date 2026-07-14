9e528aef85607975a8d48edaa2e5305e64fa44fb
Uvindu Sanjana <uvindu.sanjana@gmail.com>
2026-07-14 19:31:27 +0530

    RAG Scheduler enabled

--- services/rag/src/main/java/com/nexus/rag/RagApplication.java
+++ services/rag/src/main/java/com/nexus/rag/RagApplication.java
@@ -1,12 +1,17 @@
 package com.nexus.rag;
 
+import com.nexus.rag.infrastructure.config.IngestionSchedulerProperties;
 import org.springframework.ai.model.ollama.autoconfigure.OllamaChatAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = {OllamaChatAutoConfiguration.class})
+@EnableScheduling
+@EnableConfigurationProperties({IngestionSchedulerProperties.class})
 public class RagApplication {
 
     public static void main(String[] args) {
